b10eba7ecf808782ac30c96524d29713e9a748a9
TanvirAhmed177 <ahmed.tanvir.siddik@gmail.com>
2025-03-08 03:05:46 +0600

    feat: design updates for header footer and privacy pages

--- packages/ui-react/src/pages/Home/Home.tsx
+++ packages/ui-react/src/pages/Home/Home.tsx
@@ -2,11 +2,11 @@ import { shallow } from '@jwp/ott-common/src/utils/compare';
 import type { Content } from '@jwp/ott-common/types/config';
 import { useConfigStore } from '@jwp/ott-common/src/stores/ConfigStore';
 import { useTranslation } from 'react-i18next';
-import styles from './Home.module.scss';
 
 import ShelfList from '../../containers/ShelfList/ShelfList';
 import Button from '../../components/Button/Button';
 
+import styles from './Home.module.scss';
 const Home = () => {
   const { config } = useConfigStore(({ config, accessModel }) => ({ config, accessModel }), shallow);
   const content: Content[] = config?.content;
