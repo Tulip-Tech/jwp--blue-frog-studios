b22a3cb54749c3dcbede409bfd124f52942c9762
TanvirAhmed177 <ahmed.tanvir.siddik@gmail.com>
2025-03-19 01:18:42 +0600

    feat: added consent realated scripts

--- packages/ui-react/src/pages/Pricing/Pricing.tsx
+++ packages/ui-react/src/pages/Pricing/Pricing.tsx
@@ -1,10 +1,11 @@
 import { Check } from 'lucide-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
 import useOffers from '@jwp/ott-hooks-react/src/useOffers';
 import usePlaylist from '@jwp/ott-hooks-react/src/usePlaylist';
 import { useTranslation } from 'react-i18next';
+import { Helmet } from 'react-helmet';
 
 import Button from '../../components/Button/Button';
 import Loading from '../Loading/Loading';
@@ -15,6 +16,18 @@ import styles from './Pricing.module.scss';
 const PricingComponent = () => {
   const location = useLocation();
   const pathname = location.pathname;
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.id = 'CookieDeclaration';
+    script.src = 'https://consent.cookiebot.com/603d6ef7-9d8b-408a-81cb-0af069643a8e/cd.js';
+    script.type = 'text/javascript';
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   const { subscriptionOffers } = useOffers();
   const [selectedPlan, setSelectedPlan] = useState<'monthly' | 'annual'>('monthly');
@@ -36,103 +49,112 @@ const PricingComponent = () => {
   }
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.heading}>
-        Start your 7-day <span className={styles.italic}>free trial</span> today!
-      </h1>
-      <h1 className={styles.subHeading}>Watch your favorite concerts anytime, anywhere.</h1>
-
-      <div className={styles.plansContainer}>
-        <div
-          className={`${styles.planCard} ${selectedPlan === 'monthly' ? styles.selectedPlan : styles.annualPlan}`}
-          onClick={() => setSelectedPlan('monthly')}
-        >
-          <h2 className={styles.planTitle}>Monthly</h2>
-
-          <ul className={styles.featuresList}>
-            <li>
-              <Check className={styles.checkIcon} />
-              <span>Stream on-demand, on any device.</span>
-            </li>
-            <li>
-              <Check className={styles.checkIcon} />
-              <span>Cancel anytime during your trial - no charges, no hassle.</span>
-            </li>
-            <li>
-              <Check className={styles.checkIcon} />
-              <span>Love it? Keep watching after your free trial for just:</span>
-            </li>
-          </ul>
-
-          <div className={styles.priceContainer}>
-            <span className={styles.price}>{subscriptionOffers?.[0]?.customerPriceInclTax}</span>
-            <span className={styles.period}>/{subscriptionOffers?.[0]?.period}</span>
-          </div>
-
-          <Link className={selectedPlan === 'monthly' ? styles.monthlyButton : styles.annualButton} to={`${pathname}/?u=create-account`}>
-            Get Started
-          </Link>
-        </div>
-
-        <div className={`${styles.planCard} ${selectedPlan === 'annual' ? styles.selectedPlan : styles.annualPlan}`} onClick={() => setSelectedPlan('annual')}>
-          <div className={styles.saveBadge}>Save 16%</div>
-          <h2 className={styles.planTitle}>Annual</h2>
-          <ul className={styles.featuresList}>
-            <li>
-              <Check className={`${styles.checkIcon} ${styles.annualCheck}`} />
-              <span>Stream on-demand, on any device.</span>
-            </li>
-            <li>
-              <Check className={`${styles.checkIcon} ${styles.annualCheck}`} />
-              <span>Cancel anytime during your trial - no charges, no hassle.</span>
-            </li>
-            <li>
-              <Check className={`${styles.checkIcon} ${styles.annualCheck}`} />
-              <span>Love it? Keep watching after your free trial for just:</span>
-            </li>
-          </ul>
-          <div className={styles.priceContainer}>
-            <span className={styles.price}>{subscriptionOffers?.[1]?.customerPriceInclTax}</span>
-            <span className={styles.period}>/{subscriptionOffers?.[1]?.period}</span>
-          </div>
-          <Link className={selectedPlan === 'annual' ? styles.monthlyButton : styles.annualButton} to={`${pathname}/?u=create-account`}>
-            Get Started
-          </Link>
-        </div>
-      </div>
-      <div className={styles.textBlock}>
-        <h1 className={styles.heading} style={{ marginBottom: '3rem' }}>
-          Take your concert viewing experience to the next level
+    <>
+      {' '}
+      <Helmet>
+        <script id="CookieDeclaration" src="https://consent.cookiebot.com/603d6ef7-9d8b-408a-81cb-0af069643a8e/cd.js" type="text/javascript" async></script>
+      </Helmet>
+      <div className={styles.container}>
+        <h1 className={styles.heading}>
+          Start your 7-day <span className={styles.italic}>free trial</span> today!
         </h1>
-        {/* <PlaylistGrid data={data as Playlist} isLoading={isFetching} /> */}
+        <h1 className={styles.subHeading}>Watch your favorite concerts anytime, anywhere.</h1>
 
-        <ul className={styles.featuresList}>
-          <li>
-            <Check className={`${styles.checkIcon} ${styles.annualCheck}`} />
-            <span>BlueFrog+ makes watching your favorite concerts on demand easier than ever.</span>
-          </li>
-          <li>
-            <Check className={`${styles.checkIcon} ${styles.annualCheck}`} />
-            <span>Enjoy a sleek interface filled with features like pre-built genre playlists and advanced search options.</span>
-          </li>
-          <li>
-            <Check className={`${styles.checkIcon} ${styles.annualCheck}`} />
-            <span>With over 100 concerts available and new premieres or live streams added weekly, there&apos;s always something new to explore.</span>
-          </li>
-          <li>
-            <Check className={`${styles.checkIcon} ${styles.annualCheck}`} />
-            <span>
-              Each concert is filmed in stunning 4K resolution with multi-tracked sound, performed before an intimate audience of just 100 people, in the
-              state-of-the-art{' '}
-              <a href="https://bluefrogstudios.ca/" target="_blank" rel="noreferrer" style={{ color: '#36C5F2' }}>
-                Blue Frog Studios.
-              </a>
-            </span>
-          </li>
-        </ul>
-        <Button label="Get Started" color="primary" to={`${pathname}/?u=create-account`} />
+        <div className={styles.plansContainer}>
+          <div
+            className={`${styles.planCard} ${selectedPlan === 'monthly' ? styles.selectedPlan : styles.annualPlan}`}
+            onClick={() => setSelectedPlan('monthly')}
+          >
+            <h2 className={styles.planTitle}>Monthly</h2>
+
+            <ul className={styles.featuresList}>
+              <li>
+                <Check className={styles.checkIcon} />
+                <span>Stream on-demand, on any device.</span>
+              </li>
+              <li>
+                <Check className={styles.checkIcon} />
+                <span>Cancel anytime during your trial - no charges, no hassle.</span>
+              </li>
+              <li>
+                <Check className={styles.checkIcon} />
+                <span>Love it? Keep watching after your free trial for just:</span>
+              </li>
+            </ul>
+
+            <div className={styles.priceContainer}>
+              <span className={styles.price}>{subscriptionOffers?.[0]?.customerPriceInclTax || '0.00'}</span>
+              <span className={styles.period}>/{subscriptionOffers?.[0]?.period || 'month'}</span>
+            </div>
+
+            <Link className={selectedPlan === 'monthly' ? styles.monthlyButton : styles.annualButton} to={`${pathname}/?u=create-account`}>
+              Get Started
+            </Link>
+          </div>
+
+          <div
+            className={`${styles.planCard} ${selectedPlan === 'annual' ? styles.selectedPlan : styles.annualPlan}`}
+            onClick={() => setSelectedPlan('annual')}
+          >
+            <div className={styles.saveBadge}>Save 16%</div>
+            <h2 className={styles.planTitle}>Annual</h2>
+            <ul className={styles.featuresList}>
+              <li>
+                <Check className={`${styles.checkIcon} ${styles.annualCheck}`} />
+                <span>Stream on-demand, on any device.</span>
+              </li>
+              <li>
+                <Check className={`${styles.checkIcon} ${styles.annualCheck}`} />
+                <span>Cancel anytime during your trial - no charges, no hassle.</span>
+              </li>
+              <li>
+                <Check className={`${styles.checkIcon} ${styles.annualCheck}`} />
+                <span>Love it? Keep watching after your free trial for just:</span>
+              </li>
+            </ul>
+            <div className={styles.priceContainer}>
+              <span className={styles.price}>{subscriptionOffers?.[1]?.customerPriceInclTax || '0.00'}</span>
+              <span className={styles.period}>/{subscriptionOffers?.[1]?.period || 'year'}</span>
+            </div>
+            <Link className={selectedPlan === 'annual' ? styles.monthlyButton : styles.annualButton} to={`${pathname}/?u=create-account`}>
+              Get Started
+            </Link>
+          </div>
+        </div>
+        <div className={styles.textBlock}>
+          <h1 className={styles.heading} style={{ marginBottom: '3rem' }}>
+            Take your concert viewing experience to the next level
+          </h1>
+          {/* <PlaylistGrid data={data as Playlist} isLoading={isFetching} /> */}
+
+          <ul className={styles.featuresList}>
+            <li>
+              <Check className={`${styles.checkIcon} ${styles.annualCheck}`} />
+              <span>BlueFrog+ makes watching your favorite concerts on demand easier than ever.</span>
+            </li>
+            <li>
+              <Check className={`${styles.checkIcon} ${styles.annualCheck}`} />
+              <span>Enjoy a sleek interface filled with features like pre-built genre playlists and advanced search options.</span>
+            </li>
+            <li>
+              <Check className={`${styles.checkIcon} ${styles.annualCheck}`} />
+              <span>With over 100 concerts available and new premieres or live streams added weekly, there&apos;s always something new to explore.</span>
+            </li>
+            <li>
+              <Check className={`${styles.checkIcon} ${styles.annualCheck}`} />
+              <span>
+                Each concert is filmed in stunning 4K resolution with multi-tracked sound, performed before an intimate audience of just 100 people, in the
+                state-of-the-art{' '}
+                <a href="https://bluefrogstudios.ca/" target="_blank" rel="noreferrer" style={{ color: '#36C5F2' }}>
+                  Blue Frog Studios.
+                </a>
+              </span>
+            </li>
+          </ul>
+          <Button label="Get Started" color="primary" to={`${pathname}/?u=create-account`} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
